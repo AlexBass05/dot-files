a053fba7be39e2d95a768c4596bc9113aae19bbc
Alexander Nehrbass <alex.bass@mailbox.org>
2023-02-10 11:42:24 +0100

    Add Kitty config file

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,7 @@
  #!/usr/bin/env bash
 
+# Install  Kitty terminal emulator
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
 # Install Starship prompt: https://starship.rs
 curl -sS https://starship.rs/install.sh | sh
